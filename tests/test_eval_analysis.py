4acbd8f64ef695dd67739e97bbf2ff383edd1dac
quessy <aq15777@bristol.ac.uk>
2021-09-30 13:40:47 +0100

    changed test to be looked at during run

--- tests/test_eval_analysis.py
+++ tests/test_eval_analysis.py
@@ -1,11 +1,16 @@
 import numpy as np
 from unsupervised_skill_learning.eval_analysis import trajectory_diff, calculate_trajectory_error_stats,\
-  plot_trajectory_planner_error
+  plot_trajectory_planner_error, density_estimation
 
 
-def test_trajectory_diff():
-  actual_trajectory = np.load('test_trajectory.npy')
-  predicted_trajectory = np.load('test_predicted_trajectory.npy')
-  predicted_error = trajectory_diff(actual_trajectory, predicted_trajectory)
-  mean_error, var_error = calculate_trajectory_error_stats(predicted_error)
-  plot_trajectory_planner_error(mean_error, var_error)
+# def test_trajectory_diff():
+#   actual_trajectory = np.load('test_trajectory.npy')
+#   predicted_trajectory = np.load('test_predicted_trajectory.npy')
+#   predicted_error = trajectory_diff(actual_trajectory, predicted_trajectory)
+#   mean_error, var_error = calculate_trajectory_error_stats(predicted_error)
+#   plot_trajectory_planner_error(mean_error, var_error)
+
+
+def test_diversity():
+  trajectories = np.load('one_hot_trajectories.npy', allow_pickle=True)
+  density_estimation(trajectories)
