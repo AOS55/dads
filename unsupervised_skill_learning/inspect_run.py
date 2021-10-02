985bc12ace54026fd1cc343543c419c5dcf71fd8
quessy <aq15777@bristol.ac.uk>
2021-10-02 13:32:26 +0100

    converted to a module

--- unsupervised_skill_learning/inspect_run.py
+++ unsupervised_skill_learning/inspect_run.py
@@ -2,10 +2,16 @@ import os
 import pickle as pkl
 from dads_OOP import EnvPairs
 
-log_dir = 'log_dir/bipedal_walker_custom'
-poet_log_file = os.path.join(log_dir, 'poet_vals.pkl')
-with open(poet_log_file, 'rb') as f:
-  ea_pairs, max_poet_iters = pkl.load(f)
 
-print(ea_pairs.pairs)
-print(ea_pairs.archived_pairs)
+def get_poet_vals():
+  log_dir = 'log_dir/bipedal_walker_custom'
+  poet_log_file = os.path.join(log_dir, 'poet_vals.pkl')
+  with open(poet_log_file, 'rb') as f:
+    ea_pairs, max_poet_iters = pkl.load(f)
+  return ea_pairs, max_poet_iters
+
+
+if __name__ == '__main__':
+  ea_pairs, max_poet_iters = get_poet_vals()
+  print(ea_pairs.pairs)
+  print(ea_pairs.archived_pairs)
