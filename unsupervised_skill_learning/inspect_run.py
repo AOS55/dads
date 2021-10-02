15803bc87a6cd71854ae1af40fc846822256bfe9
quessy <aq15777@bristol.ac.uk>
2021-10-02 13:30:15 +0100

    added an inspect file to view ea_pairs

--- unsupervised_skill_learning/inspect_run.py
+++ unsupervised_skill_learning/inspect_run.py
@@ -1,6 +1,6 @@
 import os
 import pickle as pkl
-from unsupervised_skill_learning.dads_OOP import EnvPairs
+from dads_OOP import EnvPairs
 
 log_dir = 'log_dir/bipedal_walker_custom'
 poet_log_file = os.path.join(log_dir, 'poet_vals.pkl')
